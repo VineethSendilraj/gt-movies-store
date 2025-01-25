7e04e74884596959ad9f6878bcb2bc3c9bb6c09d
Vineeth Sendilraj <vineethsendilraj@gmail.com>
2025-01-25 17:45:21 -0500

    Made website responsive

--- gt_movies_store/settings.py
+++ gt_movies_store/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'gt_movies_store.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [os.path.join(BASE_DIR, 'gt_movies_store/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -123,3 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom settings
+STATICFILES_DIRS = [
+    BASE_DIR / 'gt_movies_store/static',
+]
